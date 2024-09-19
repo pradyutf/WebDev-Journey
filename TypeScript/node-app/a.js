b3554186e444de2456a5648eec924438d15da2db
pradyutf <pradyutfogla@gmail.com>
2024-09-20 02:56:06 +0530

    basics

---
+++ TypeScript/node-app/a.js
@@ -0,0 +1,7 @@
+"use strict";
+const x = 1;
+console.log(x);
+function greet_user(str) {
+    console.log('Hello ' + str);
+}
+greet_user("Pradyut");
--- TypeScript/node-app/a.ts
+++ TypeScript/node-app/a.ts
@@ -1,2 +1,8 @@
 const x: number = 1;
-console.log(x);
+console.log(x); 
+
+function greet_user (str: string){
+    console.log('Hello '+str);
+}
+
+greet_user("Pradyut")
