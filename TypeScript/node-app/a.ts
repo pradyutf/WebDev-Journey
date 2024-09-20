b02328db6317aa1afc5d09c1ba92dda8886e58bf
pradyutf <pradyutfogla@gmail.com>
2024-09-20 20:25:09 +0530

    basics

--- TypeScript/node-app/a.ts
+++ TypeScript/node-app/a.ts
@@ -1,8 +1,17 @@
 const x: number = 1;
 console.log(x); 
 
-function greet_user (str: string){
+function greetUser (str: string){
     console.log('Hello '+str);
 }
+function sum (a:number, b:number): number{
+ return a+b;
+}
 
-greet_user("Pradyut")
+function returnsFunction (fn: (a:number, b:number)=> number){
+    fn(1,2);
+}
+console.log(returnsFunction(sum));
+
+
+greetUser("Pradyut")
