81672dbfba51fa638bf5d344164109e81b502bb2
pradyutf <pradyutfogla@gmail.com>
2024-12-10 00:17:52 +0530

    createUsers

--- week10.2/src/index.ts
+++ week10.2/src/index.ts
@@ -1,4 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+import { log } from "console";
 const prisma = new PrismaClient();
 
-prisma.user
+
+
+async function insertUser (email: string, firstName: string, lastName: string, password: string){
+    const res = await prisma.user.create({
+        data:{
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: password,
+            userId: 1
+        },
+        select:{
+            id: true,
+            firstName: true
+        }
+        
+    })
+
+    console.log(res);
+    
+}
+
+insertUser("px@gmail.com", "Pradyut", "Fogla", "yoyo");
+
+ 
